8a2ec0683ac9b5e5311b7c5cc31e9344979d0110
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-10-26 11:41:43 -0400

    updated search

--- src/Components/SearchResultsTable.js
+++ src/Components/SearchResultsTable.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import API from '../utils/API';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Table} from 'react-bootstrap';
 import MovieSearchForm from './MovieSearchForm.js'
-import { Table } from 'react-bootstrap';
 import "./stylesheet.css";
 
 class SearchResultsTable extends Component {
@@ -11,13 +10,13 @@ class SearchResultsTable extends Component {
         search: ""
     };
 
+searchMovie = query => {
+API.search(query)
+.then(res => this.setState ({ result: res.data }))
 
-//Function to query the movie database
-searchMovie= query => {
-    API.search(query)
-        .then(res => this.setState({ result: res.data }))
-        .catch(err => console.log (err));
-};
+ .catch(err => console.log (err));
+
+}
 
 //capture the text as entered by the user
 handleInputChange = event => {
@@ -26,15 +25,51 @@ handleInputChange = event => {
     this.setState({
         [name]: value
     });
+    console.log(value)
 };
 
-//On form submission, search movie database using captured text
+
+
 handleFormSubmit = event => {
     event.preventDefault();
-    this.searchMovie(this.state.search);
-    console.log(this.state.search);
+   this.searchMovie(this.state.search);
+    if (this.state.search.length > 0) {
+        console.log("No. of results:" + this.state.search.search.length);
+        console.log(this.state.result);
+      }
+      else {
+          alert("Please enter a movie title to search")
+          console.log("nothing to search")}
+  
 };
 
+
+movieTable = () => (
+    <Table striped bordered className="movieTable">
+            <thead>
+                <tr>
+                    <th>Movie Poster</th>
+                    <th>Title</th>
+                    <th>Year</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.result.length ? (
+                    this.state.result.map(result => (
+                <tr key={result.imdbID}> 
+                    <td><img src={result.Poster} alt={this.state.result.Title} style={{width:"50%"}}></img></td>
+                    <td>{result.Title}</td>
+                    <td>{result.Year}</td>
+                </tr> 
+    ))
+    ) : (
+        <tr />
+    )}
+            </tbody> 
+              
+        </Table> 
+       
+);
 
 //Display search box in jumbotron and results in table form
 render () {
@@ -56,26 +91,29 @@ render () {
                 <tr>
                     <th>Movie Poster</th>
                     <th>Title</th>
-                    <th>Plot Summary</th>
-                    <th>Rating</th>
-                    <th>Director</th>
-                    <th>Release Date</th>
+                    <th>Year</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td><img src={this.state.result.Poster} alt={this.state.result.Title} style={{width:"50%"}}></img></td>
-                    <td>{this.state.result.Title}</td>
-                    <td>{this.state.result.Plot}</td>
-                    <td>{this.state.result.Rated}</td>
-                    <td>{this.state.result.Director}</td>
-                    <td>{this.state.result.Released}</td>
+                {this.state.result.length ? (
+                    this.state.result.map(result => (
+                <tr key={result.imdbID}> 
+                    <td><img src={result.Poster} alt={this.state.result.Title} style={{width:"50%"}}></img></td>
+                    <td>{result.Title}</td>
+                    <td>{result.Year}</td>
                 </tr> 
+    ))
+    ) : (
+        <tr />
+    )}
             </tbody> 
-        </Table>
+              
+        </Table> 
+        
     </div>  
 );
 }
 }
+
 
 export default SearchResultsTable;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const BASEURL = "https://www.omdbapi.com/?t=";
+const BASEURL = "https://www.omdbapi.com/?s="
 const APIKEY = "&apikey=9f572b90";
 
 //export search method to query movie API for movie that is entered by user
 export default {
     search: function(query) {
-        return axios.get(BASEURL + query + APIKEY);
-    }
-};
+      console.log("API query in progress");
+        return axios.get(BASEURL + query + APIKEY)
+          }
+        };
